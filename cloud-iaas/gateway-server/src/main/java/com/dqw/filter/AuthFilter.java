c51b6dbda62989eb6e8b5d46a0ec7a960fcfd75a
dqw666 <9562833+deng-qingwen@user.noreply.gitee.com>
2024-06-18 20:04:52 +0800

    添加网页前端代码

--- cloud-iaas/gateway-server/src/main/java/com/dqw/filter/AuthFilter.java
+++ cloud-iaas/gateway-server/src/main/java/com/dqw/filter/AuthFilter.java
@@ -1,4 +1,15 @@
 package com.dqw.filter;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/*
+* 全局TOKEN过滤器
+* */
+@Slf4j
+@Component
 public class AuthFilter {
+
+
 }
